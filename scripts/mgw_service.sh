6573fabb3cf4b33aa83a9999741ea6925e344bcc
y-du <42994541+y-du@users.noreply.github.com>
2022-08-18 16:48:24 +0200

    return error if no serial given

--- scripts/mgw_service.sh
+++ scripts/mgw_service.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 sed -i "s/__PREFIX__/${MGW_NAME_PREFIX:-}/g" $AVAHI_SRV_PATH/mgw.service
+
+if [ -z "${MGW_SERIAL}" ]; then
+  >&2 echo "missing serial number"
+  exit 61
+fi
+
 sed -i "s/__SERIAL__/$MGW_SERIAL/g" $AVAHI_SRV_PATH/mgw.service

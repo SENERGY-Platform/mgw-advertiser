f0c0cc8a458cc1a38d7c6ce08ac07ab2e55d5184
y-du <42994541+y-du@users.noreply.github.com>
2022-08-18 13:29:22 +0200

    check if container init and use exec to receive signals

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 changeConfOption() {
     if [ -n "$(grep -E $1 $3)" ]; then
         echo "set $1 -> $2"
@@ -9,19 +7,20 @@ changeConfOption() {
     fi
 }
 
-
 CONFIG=/etc/avahi/avahi-daemon.conf
 
-changeConfOption "enable-dbus" "no" $CONFIG
+if [ ! -f "init" ]; then
+    changeConfOption "enable-dbus" "no" $CONFIG
+	for env_var in $(env); do
+	    if [ -n "$(echo $env_var | grep -E '^AD_')" ]; then
+		name=$(echo "$env_var" | sed -r "s/AD_([^=]*)=.*/\1/g" | sed -e "s/\(.*\)/\L\1/" | sed -e 's:_:-:g')
+		env_var_name=$(echo "$env_var" | sed -r "s/([^=]*)=.*/\1/g")
+		value=$(printenv $env_var_name)
+		changeConfOption $name $value $CONFIG
+	    fi
+	done
+	touch init
+fi
 
-for env_var in $(env); do
-    if [ -n "$(echo $env_var | grep -E '^AD_')" ]; then
-        name=$(echo "$env_var" | sed -r "s/AD_([^=]*)=.*/\1/g" | sed -e "s/\(.*\)/\L\1/" | sed -e 's:_:-:g')
-        env_var_name=$(echo "$env_var" | sed -r "s/([^=]*)=.*/\1/g")
-        value=$(printenv $env_var_name)
-        changeConfOption $name $value $CONFIG
-    fi
-done
-
-avahi-daemon
+exec avahi-daemon
 
